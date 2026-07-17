d0bc99869202a7e1f2be4d353304b3fe08270d3f
Ilkham <valitovilkham@gmail.com>
2026-07-17 21:39:02 +0400

    created Screen

--- app/components/Device/Device.tsx
+++ app/components/Device/Device.tsx
@@ -1,9 +1,10 @@
 import styles from "./Device.module.scss"
+import Screen from "@/app/components/Screen/Screen"
 
 export default function Device() {
     return (
         <div className={styles.device}>
-            
+            <Screen />
         </div>
     )
 }
---
+++ app/components/Screen/Screen.tsx
@@ -0,0 +1,9 @@
+import styles from "./Screen.module.scss"
+
+export default function Screen() {
+    return (
+        <div className={styles.screen}>
+            
+        </div>
+    )
+}
