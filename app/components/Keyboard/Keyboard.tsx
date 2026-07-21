30fd6ae0918cc7614293193d80139b3917b4b4b4
Ilkham <valitovilkham@gmail.com>
2026-07-21 22:12:53 +0400

    created Operators

--- app/components/Keyboard/Keyboard.tsx
+++ app/components/Keyboard/Keyboard.tsx
@@ -1,9 +1,10 @@
 import styles from "./Keyboard.module.scss"
+import Operators from "../Operators/Operators"
 
 export default function Keyboard() {
     return (
         <div className={styles.keyboard}>
-            
+            <Operators />
         </div>
     )
 }
