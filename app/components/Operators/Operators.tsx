4c5546a0325225de85d3efa66dd3903f0185093e
Ilkham <valitovilkham@gmail.com>
2026-08-04 18:09:16 +0400

    minor changes

--- app/components/Operators/Operators.tsx
+++ app/components/Operators/Operators.tsx
@@ -1,6 +1,7 @@
 import styles from "./Operators.module.scss"
 import Plus from "../Plus/Plus"
 import Minus from "../Minus/Minus"
+import Multiplication from "../Multiplication/Multiplication"
 
 export default function Operators() {
     return (
