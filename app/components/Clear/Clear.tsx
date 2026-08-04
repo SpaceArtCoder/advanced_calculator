ace3cfc3af3839b35329828d97951e84082f44a9
Ilkham <valitovilkham@gmail.com>
2026-08-04 18:35:02 +0400

    created Clear

---
+++ app/components/Clear/Clear.tsx
@@ -0,0 +1,7 @@
+import styles from './Clear.module.scss'
+
+export default function Clear() {
+    return (
+        <button className={styles.clear}>C</button>
+    )
+}
--- app/components/Control/Control.tsx
+++ app/components/Control/Control.tsx
@@ -1,7 +1,10 @@
 import styles from "./Control.module.scss"
+import Clear from "../Clear/Clear"
 
 export default function Control() {
     return (
-        <div></div>
+        <div className={styles.control}>
+            <Clear />
+        </div>
     )
 }
