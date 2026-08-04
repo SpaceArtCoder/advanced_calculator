873f7958cb218faa21e0b202628948d8f2fbcc6c
Ilkham <valitovilkham@gmail.com>
2026-08-04 18:48:45 +0400

    created OnOff

--- app/components/Control/Control.tsx
+++ app/components/Control/Control.tsx
@@ -1,10 +1,12 @@
 import styles from "./Control.module.scss"
 import Clear from "../Clear/Clear"
+import OnOff from "../OnOff/OnOff"
 
 export default function Control() {
     return (
         <div className={styles.control}>
             <Clear />
+            <OnOff />
         </div>
     )
 }
---
+++ app/components/OnOff/OnOff.tsx
@@ -0,0 +1,7 @@
+import styles from './OnOff.module.scss'
+
+export default function OnOff() {
+    return (
+        <button className={styles.onoff}>On/Off</button>
+    )
+}
