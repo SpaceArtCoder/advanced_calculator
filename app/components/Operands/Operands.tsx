bf3822208afc36e1427e978d9a39630e96505491
Ilkham <valitovilkham@gmail.com>
2026-08-06 22:36:57 +0400

    created One

--- app/components/Operands/Operands.tsx
+++ app/components/Operands/Operands.tsx
@@ -1,7 +1,10 @@
 import styles from "./Operands.module.scss"
+import One from "../One/One"
 
 export default function Operands() {
     return (
-        <div className={styles.operands}></div>
+        <div className={styles.operands}>
+            <One />
+        </div>
     )
 }
