c99d672475a0d5a87b76e92c3de3a9ca880e7ad0
Ilkham <valitovilkham@gmail.com>
2026-08-06 22:42:40 +0400

    created Three

--- app/components/Operands/Operands.tsx
+++ app/components/Operands/Operands.tsx
@@ -1,12 +1,14 @@
 import styles from "./Operands.module.scss"
 import One from "../One/One"
 import Two from "../Two/Two"
+import Three from "../Three/Three"
 
 export default function Operands() {
     return (
         <div className={styles.operands}>
             <One />
             <Two />
+            <Three />
         </div>
     )
 }
---
+++ app/components/Three/Three.tsx
@@ -0,0 +1,7 @@
+import styles from '@/app/components/Keyboard/Keyboard.module.scss'
+
+export default function Three() {
+    return (
+        <button className={styles.one}>3</button>
+    )
+}
