b240dab1293c94f6c34c1c11fa1aeba2a9e148fd
Ilkham <valitovilkham@gmail.com>
2026-08-06 22:40:08 +0400

    created Two

--- app/components/Operands/Operands.tsx
+++ app/components/Operands/Operands.tsx
@@ -1,10 +1,12 @@
 import styles from "./Operands.module.scss"
 import One from "../One/One"
+import Two from "../Two/Two"
 
 export default function Operands() {
     return (
         <div className={styles.operands}>
             <One />
+            <Two />
         </div>
     )
 }
---
+++ app/components/Two/Two.tsx
@@ -0,0 +1,7 @@
+import styles from '@/app/components/Keyboard/Keyboard.module.scss'
+
+export default function Two() {
+    return (
+        <button className={styles.two}>2</button>
+    )
+}
