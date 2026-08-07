4cda80f95d100d0a16ac6ccad95647f219580066
Ilkham <valitovilkham@gmail.com>
2026-08-07 16:32:52 +0400

    added Greeting display state for Screen

--- app/components/Screen/Screen.tsx
+++ app/components/Screen/Screen.tsx
@@ -1,10 +1,14 @@
 import styles from "./Screen.module.scss"
 import Greeting from "../Greeting/Greeting"
+import { useState } from "react"
 
 export default function Screen() {
+
+    const [showHello, setShowHello] = useState(false); //Greeting word isn't shown by default only after clicking the on/off button
+
     return (
         <div className={styles.screen}>
-            <Greeting />
+            <Greeting showHello = {showHello}/>
         </div>
     )
 }
