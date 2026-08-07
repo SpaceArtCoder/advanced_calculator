9726665bc6d5d5c755d0839291da4e212f12bdc0
Ilkham <valitovilkham@gmail.com>
2026-08-07 16:16:23 +0400

    added fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,13 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.scss";
+
+
+const digitalFont = localFont({
+  src: "./fonts/BitcountPropSingle-VariableFont_CRSV,ELSH,ELXP,slnt,wght.ttf",
+  variable: "--font-digital",
+})
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,7 +30,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
+    <html lang="en" className={`${geistSans.variable} ${geistMono.variable} ${digitalFont.variable}`}>
       <body>{children}</body>
     </html>
   );
