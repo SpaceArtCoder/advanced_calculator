a98974e6b5ec9e4bb9d18c1cf58098eaffcfd379
Ilkham <valitovilkham@gmail.com>
2026-08-12 21:55:24 +0400

    defined type for app state and actions

--- app/store/useCalculatorStore.ts
+++ app/store/useCalculatorStore.ts
@@ -1,2 +1,8 @@
 import {create} from 'zustand'
 
+// Define the type for state and actions
+interface CalculatorState {
+    showHello: boolean;
+    toggleShowHello: () => void;
+    setShowHello: (value: boolean) => void;
+}
