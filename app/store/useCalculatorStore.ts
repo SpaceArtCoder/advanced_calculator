a1a565292cf0fa033b18e3ffc9ff8c27d39ea955
Ilkham <valitovilkham@gmail.com>
2026-08-12 22:05:03 +0400

    created zustand store

--- app/store/useCalculatorStore.ts
+++ app/store/useCalculatorStore.ts
@@ -1,8 +1,21 @@
 import {create} from 'zustand'
 
-// Define the type for state and actions
+// Type for state and actions
 interface CalculatorState {
     showHello: boolean;
     toggleShowHello: () => void;
     setShowHello: (value: boolean) => void;
 }
+
+// Store
+export const useCalculatorStore = create<CalculatorState>((set) => ({
+    // State variable
+    showHello: false,
+
+    // Action to flip boolean value
+    toggleShowHello: () => set((state) => ({showHello: !state.showHello})),
+
+    // Action to explicitly set true or false
+    setShowHello: (value: boolean) => set({showHello: value}),
+
+}));
