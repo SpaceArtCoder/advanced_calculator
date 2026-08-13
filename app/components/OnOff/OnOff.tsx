5849bdadd46db4972fd0e34b69e21efbe7fc1f5d
Ilkham <valitovilkham@gmail.com>
2026-08-13 11:04:28 +0400

    added store variable for OnOff

--- app/components/OnOff/OnOff.tsx
+++ app/components/OnOff/OnOff.tsx
@@ -1,8 +1,13 @@
-// import styles from './OnOff.module.scss'
+'use client'
+
 import styles from '@/app/components/Keyboard/Keyboard.module.scss'
+import { useCalculatorStore } from '@/app/store/useCalculatorStore'
 
 export default function OnOff() {
+
+    const toggleShowHello = useCalculatorStore((state) => state.toggleShowHello);
+
     return (
-        <button className={styles.onoff}>On/Off</button>
+        <button className={styles.onoff} onClick={toggleShowHello}>On/Off</button>
     )
 }
--- app/components/Screen/Screen.tsx
+++ app/components/Screen/Screen.tsx
@@ -5,7 +5,7 @@ import Greeting from "../Greeting/Greeting"
 import { useCalculatorStore } from "@/app/store/useCalculatorStore"
 
 export default function Screen() {
-
+ 
     // Read state and grab actions
     const showHello = useCalculatorStore((state) => state.showHello);
     const toggleShowHello = useCalculatorStore((state) => state.toggleShowHello);
