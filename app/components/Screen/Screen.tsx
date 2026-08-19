c425a652d845ff81e67b4e7789990779ad3f0630
Ilkham <valitovilkham@gmail.com>
2026-08-19 21:50:58 +0400

    added timer for greeting

--- app/components/Screen/Screen.tsx
+++ app/components/Screen/Screen.tsx
@@ -3,11 +3,27 @@
 import styles from "./Screen.module.scss"
 import Greeting from "../Greeting/Greeting"
 import { useCalculatorStore } from "@/app/store/useCalculatorStore"
+import { useEffect } from "react"
 
 export default function Screen() {
  
     // Read state and grab actions
     const showHello = useCalculatorStore((state) => state.showHello);
+
+    const toggleShowHello = useCalculatorStore((state) => state.toggleShowHello);
+
+
+    useEffect(() => {
+        // Only start a timer if showHello is currently true
+        if (!showHello) return;
+
+        const interval = setTimeout(() => {
+            toggleShowHello();
+        }, 5000);
+
+        return () => clearTimeout(interval);
+
+    }, [showHello, toggleShowHello]);
 
     return (
         <div className={styles.screen}>
