ce9cbaba97a85b514c4b1635c780364d6cf3f5a3
Ilkham <valitovilkham@gmail.com>
2026-08-20 22:37:22 +0400

    minor changes

--- app/components/OnOff/OnOff.tsx
+++ app/components/OnOff/OnOff.tsx
@@ -2,12 +2,21 @@
 
 import styles from '@/app/components/Keyboard/Keyboard.module.scss'
 import { useCalculatorStore } from '@/app/store/useCalculatorStore'
+import { useEffect } from 'react';
 
 export default function OnOff() {
 
     const toggleShowHello = useCalculatorStore((state) => state.toggleShowHello);
 
-    const tooglePower = useCalculatorStore((state) => state.togglePower)
+    const tooglePower = useCalculatorStore((state) => state.togglePower);
+
+    const power = useCalculatorStore((state) => state.power);
+
+
+    useEffect(() => {
+        if (power) toggleShowHello();
+
+    }, [power])
 
     return (
         // Turn on/off calculator button
