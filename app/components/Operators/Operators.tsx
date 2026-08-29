f59d8df2a973d010fef6f345223dcee4e8bf95d1
SpaceArtCoder <Valitovilkham@gmail.com>
2026-08-29 16:17:44 +0400

    removed extra components for arithmetic signs in Operators

--- app/components/Operators/Operators.tsx
+++ app/components/Operators/Operators.tsx
@@ -1,4 +1,5 @@
 import styles from "./Operators.module.scss"
+
 import Plus from "../Plus/Plus"
 import Minus from "../Minus/Minus"
 import Multiplication from "../Multiplication/Multiplication"
@@ -6,13 +7,14 @@ import Division from "../Division/Division"
 import Radical from "../Radical/Radical"
 
 export default function Operators() {
+
+    const signArray = ['+','-','*','/'];
+  
     return (
         <div className={styles.operators}>
-            <Plus />
-            <Minus />
-            <Multiplication />
-            <Division />
-            <Radical />
+          {signArray.map((sign, index) => (
+          <button key={index} className={styles.signs}>{sign}</button>
+          ))}
         </div>
     )
 }
