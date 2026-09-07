406d3eef413cdf23031dcbe5a36701c2df6218ab
Ilkham <valitovilkham@gmail.com>
2026-09-07 14:00:22 +0400

    added blinking effect for BlinkingCursor

--- app/components/BlinkingCursor/BlinkingCursor.tsx
+++ app/components/BlinkingCursor/BlinkingCursor.tsx
@@ -1,7 +1,23 @@
+'use client'
+
+import { useEffect, useState } from 'react'
 import styles from './BlinkingCursor.module.scss'
 
 export default function BlinkingCursor() {
+
+    const [visibility, setVisibility] = useState(true);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setVisibility((current) => !current)
+        }, 500);
+        
+        return () => clearInterval(interval);
+    }, []);
+
     return (
-        <output>_</output>
+        <output className={visibility ? styles.visible : styles.hidden}>
+            _
+        </output>
     )
 }
