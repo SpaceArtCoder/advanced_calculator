5d53d2331f7fb0fdd908bf430ee83282cb3cfcce
Ilkham <valitovilkham@gmail.com>
2026-09-08 18:32:40 +0400

    added clearing ability for Clear

--- app/components/Clear/Clear.tsx
+++ app/components/Clear/Clear.tsx
@@ -1,8 +1,14 @@
+'use client'
+
 // import styles from './Clear.module.scss'
 import styles from '@/app/components/Keyboard/Keyboard.module.scss'
+import { useCalculatorStore } from '@/app/store/useCalculatorStore'
 
 export default function Clear() {
+
+    const clearFinalExpression = useCalculatorStore((state) => state.clearFinalExpression);
+
     return (
-        <button className={styles.clear}>C</button>
+        <button className={styles.clear} onClick={() => clearFinalExpression()}>C</button>
     )
 }
