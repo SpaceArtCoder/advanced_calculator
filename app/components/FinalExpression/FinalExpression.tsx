1122ed23379e3bf7dada04ed823ff5a93fe274fb
Ilkham <valitovilkham@gmail.com>
2026-09-08 20:46:06 +0400

    minor changes

--- app/components/FinalExpression/FinalExpression.tsx
+++ app/components/FinalExpression/FinalExpression.tsx
@@ -1,14 +1,20 @@
 import styles from './FinalExpression.module.scss'
 import { useCalculatorStore } from '@/app/store/useCalculatorStore'
 import BlinkingCursor from '@/app/components/BlinkingCursor/BlinkingCursor'
+import { useEffect } from 'react';
 
 export default function FinalExpression() {
 
     const finalExpression = useCalculatorStore((state) => state.finalExpression);
 
-    function calculator() {
-        
-    }
+    useEffect(() => {
+        function calculator() {
+            console.log(finalExpression.indexOf('+', 0));
+        }
+
+        calculator();
+    })
+    
 
     return (
         // The blinking cursor is displayed only when the field is empty
