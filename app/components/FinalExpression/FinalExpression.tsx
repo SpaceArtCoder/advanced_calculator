5d1c7a26c75894676ed74c3786f4d14df668c9f6
SpaceArtCoder <Valitovilkham@gmail.com>
2026-09-19 18:23:09 +0400

    minor changes

--- app/components/FinalExpression/FinalExpression.tsx
+++ app/components/FinalExpression/FinalExpression.tsx
@@ -9,6 +9,7 @@ export default function FinalExpression() {
 
     // Contains everything entered by user
     const finalExpression = useCalculatorStore((state) => state.finalExpression);
+    
     console.log(finalExpression);
     
     return (
@@ -16,5 +17,11 @@ export default function FinalExpression() {
         <output className={styles.input}>{finalExpression.length ? finalExpression : <BlinkingCursor />}</output>
     )
 }
-
+// Сюда подставляется переменная состояния temporaryBuffer
+// Он будет вместо finalExpression до тех пор пока не введен
+// оператор. То есть, после набора оператора temporaryBuffer
+// передает свое содержимое в finalExpression, а затем очищается.
+// Далее, возможно, все повторится снова, при наборе операндов
+// будет отображатся temporaryBuffer, но до ввода следующего
+// оператора
 // 2+2*2-7+8+7-6/4*3+2/9+2
